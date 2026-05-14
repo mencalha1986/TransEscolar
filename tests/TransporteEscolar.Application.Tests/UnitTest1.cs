925e5e99423233ee2885839990de66e64d8705ee
romulo.costa <mencalha1986@gmail.com>
2026-05-14 20:30:20 -0300

    chore: scaffold solution with Clean Architecture layers (.NET 10)

---
+++ tests/TransporteEscolar.Application.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TransporteEscolar.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/TransporteEscolar.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TransporteEscolar.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
